cc0a8b2b64fa4f22b513a03b401ac0ee9841048d
mnia <marcos.ferreira@gft.com>
2022-03-21 13:19:57 -0300

    Fim da aula 12 - Salvando Categoria

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,14 +1,34 @@
+using bluemarket.Data;
 using bluemarket.DTO;
+using bluemarket.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bluemarket.Controllers
 {
     public class CategoriasController : Controller
     {
+        private readonly ApplicationDbContext database;
+        public CategoriasController(ApplicationDbContext database)
+        {
+            this.database = database;
+        }
+
         [HttpPost]
         public IActionResult Salvar(CategoriaDTO categoriaTemporaria)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Categoria categoria = new Categoria();
+                categoria.Nome = categoriaTemporaria.Nome;
+                categoria.Status = true;
+                database.Categorias.Add(categoria);
+                database.SaveChanges();
+                return RedirectToAction("Categorias", "Admin");
+            }
+            else
+            {
+                return View("../Admin/NovaCategoria");
+            }
         }
     }
 }
