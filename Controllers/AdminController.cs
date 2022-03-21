14b1179fac527c4d90d735dcbf2f8d2f7e8d452a
mnia <marcos.ferreira@gft.com>
2022-03-21 15:21:19 -0300

    Aula 19 - Criando lista de categorias

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -18,7 +18,8 @@ namespace bluemarket.Controllers
 
         public IActionResult Categorias()
         {
-            return View();
+            var categorias = database.Categorias.ToList();
+            return View(categorias);
         }
 
         public IActionResult NovaCategoria()
